a90e713f11ae905c836560867f095147581e9677
Boreka <borisbadashvili@gmail.com>
2018-07-14 00:24:44 +0400

    Almost Final

--- Table (Project)/Project.js
+++ Table (Project)/Project.js
@@ -1,109 +1,77 @@
 // Date Object
-
-let myDate = new Date(2018, 3, 30)
-console.log(myDate)
-let dateNumber = myDate.getDate()
-let monthNumber = myDate.getMonth()
-let dayNumber = myDate.getDay()
-
-let newdate = dateNumber + "</br>" + monthNumber + "</br>" + dayNumber
-
-
-
-
-
-
-
+// let myDate = new Date(2018, 3, 30)
+// console.log(myDate)
+// let dateNumber = myDate.getDate()
+// let monthNumber = myDate.getMonth()
+// let dayNumber = myDate.getDay()
+// let newdate = dateNumber + "</br>" + monthNumber + "</br>" + dayNumber
 // Month number into Month Name
-function monthNumberToMonthNumes(monthNumber) {
-
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return monthNames[monthNumber];
-}
 
 
+// function monthNumberToMonthNumes(monthNumber) {
 
-// Week number into WeekDay Name
-
-function weekNumberToWeekName(dayNumber) {
-    var weekDay = ["Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur"];
-    return weekDay[dayNumber];
-}
-
+//     var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     return monthNames[monthNumber];
+// }
+// function weekNumberToWeekName(dayNumber) {
+//     var weekDay = ["Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur"];
+//     return weekDay[dayNumber];
+// }
 
 
 //  addDay Button function  
 
 function addDay() {
-
-    for (k = 1; k < 11; k++) {
+// For loop to create 10 div elements. (For Grades)
+    for (k = 1; k < 11; k++) {     
         let div = document.createElement("div");
-        // div.style.background = "red"
-        // div.style.color = "white"
-        // div.style.width = "40px"
-        // div.style.height = "20px"
-        // div.style.margin = "0.5px"
-        // div.style.textAlign = "center"
-        // div.style.borderRadius = "6px"
         div.setAttribute("class", "studentGrades")
         div.className += " sgID" + k
-        div.setAttribute("onclick", "averageFunc(this, Number(prompt('Please, enter number here')))");
-
+        div.setAttribute("onclick", "userInput(this, Number(prompt('Please, enter number here')))");
         div.innerHTML = "0"
-
         document.querySelector("#container3").appendChild(div)
     }
-    let dateDiv = document.createElement("div");
+// Creates 1 div element. (For Date)
+    let dateDiv = document.createElement("div"); 
     dateDiv.setAttribute("class", "dateDiv");
-    dateDiv.style.background = "green"
-    dateDiv.style.color = "white"
-    dateDiv.style.width = "40px"
-    dateDiv.style.height = "20px"
-    dateDiv.style.margin = "0.5px"
-    dateDiv.style.textAlign = "center"
-    dateDiv.style.borderRadius = "6px"
-    dateDiv.style.fontSize = "0.5em"
 
     dateDiv.innerHTML = "sup"
 
     document.querySelector("#container3").appendChild(dateDiv)
 
     averageGrade()
+
 }
+document.getElementById("add").addEventListener("click", addDay); // eventListener for AddDay Button, Calls addDay function.
 
-document.getElementById("add").addEventListener("click", addDay);
 
-function averageFunc(oreo, todaysResult) {
+// Function for userInput, changes color if number is inputed also, rounds and binds numbers to certain logic. (0.5 = 1... etc.)
+function userInput(userInputDiv, todaysResult) {
     if (todaysResult > 5) {
-        oreo.innerHTML = 5;
-        oreo.style.background = "green";
-
-        // if students result for current day is from 0, till 5, write rounded result.
-    } else if (todaysResult <= 5 && todaysResult >= 0) {
-        oreo.innerHTML = (Math.round(todaysResult));
-        oreo.style.background = "green";
+        userInputDiv.innerHTML = 5;
+        userInputDiv.style.background = "darkblue";
+       
+    } else if (todaysResult <= 5 && todaysResult >= 0) { 
+        userInputDiv.innerHTML = (Math.round(todaysResult));
+        userInputDiv.style.background = "darkblue";
 
     }
     else {
-        oreo.innerHTML = 0;
+        userInputDiv.innerHTML = 10;
     }
 
     averageGrade()
-
 }
 
-
-
-// studentAvg
+// averageGrade calculates 
 
 function averageGrade() {
-
-
+debugger;
     let totalDays = document.querySelectorAll(".dateDiv").length
     let studentsAvg = Array.from(document.querySelectorAll(".average"))
 
     for (let j = 0; j < studentsAvg.length; j++) {
-        let grades = Array.from(document.querySelectorAll(".sgID" + ( j+ 1 )))
+        let grades = Array.from(document.querySelectorAll(".sgID" + (j + 1)))
         let sum = 0
         for (let z = 0; z < totalDays; z++) {
             sum += Number(grades[z].textContent)
@@ -112,9 +80,7 @@ function averageGrade() {
     }
 }
 
-
 // removeDay Button function
-
 
 function removeDay() {
 
@@ -122,7 +88,6 @@ function removeDay() {
         let removeDay = document.querySelector('.studentGrades');
         removeDay.parentNode.removeChild(removeDay);
     }
-
     let removeDate = document.querySelector(".dateDiv")
     removeDate.parentNode.removeChild(removeDate)
 }
