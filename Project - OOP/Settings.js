4f737f997332b80cdce31a49361497ea5ebfd46f
Boreka <borisbadashvili@gmail.com>
2018-09-30 04:55:13 +0400

    ww

--- Project - OOP/Settings.js
+++ Project - OOP/Settings.js
@@ -17,17 +17,19 @@ let foodQuantity = document.querySelector("#food")
 let foodAmount
 
 //  --------------------------------------------------------------------------------------
-window.onload = function() {
+window.onload = function () {
     highScore = storage.getItem('highScore')
     document.querySelector("#score").innerHTML = "Highest Score is : " + highScore
 }
 
 // BoardOMeter -- Checking if there is avaliable value for (board W/H) in LocalStorage
 let boardOMeter = function () {
-    canvasWidth = parseInt(localStorage.getItem("boardW"))
-    canvasHeight = parseInt(localStorage.getItem("boardH"))
-    canvas.width = canvasWidth
-    canvas.height = canvasHeight
+    if (localStorage.getItem('speed') !== null) {
+        canvasWidth = parseInt(localStorage.getItem("boardW"))
+        canvasHeight = parseInt(localStorage.getItem("boardH"))
+        canvas.width = canvasWidth
+        canvas.height = canvasHeight
+    }
 }
 // SpeedOMeter -- Checking if there is avaliable value for speed in LocalStorage
 let speedOMeter = function () {
@@ -58,15 +60,15 @@ let SnakeOMeter = function () {
 }
 
 // FoodOMeter --  Checking if there is avaliable value for FoodQuantity in LocalStorage
-let foodOMeter = function() {
-    if(localStorage.getItem('foodQuantity') !== null) {
+let foodOMeter = function () {
+    if (localStorage.getItem('foodQuantity') !== null) {
         foodAmount = parseInt(storage.getItem('foodQuantity'))
     } else {
         foodAmount = 1
     }
 }
 // ScoreOMeter -- Checking if there is avaliable value for HighScore in LocalStorage
-let scoreOMeter = function() {
+let scoreOMeter = function () {
 
 }
 
